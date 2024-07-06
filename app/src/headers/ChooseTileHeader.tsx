80f1eae34072305138b0b8dfeb5d11778bd2a1f1
Denis <denis.boulanger@yahoo.com>
2024-07-06 10:52:41 +0200

    Adapted header after placing a tile

--- app/src/headers/ChooseTileHeader.tsx
+++ app/src/headers/ChooseTileHeader.tsx
@@ -1,16 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { Memory } from '@gamepark/rivality/rules/Memory'
 import { RivalityRules } from '@gamepark/rivality/RivalityRules'
 import { useTranslation } from 'react-i18next'
 
 export const ChooseTileHeader = () => {
   const { t } = useTranslation()
   const playerId = usePlayerId()
-  const activePlayer = useRules<RivalityRules>()?.game.rule?.player
+  const rules=useRules<RivalityRules>()
+  const activePlayer = rules?.game.rule?.player
   const player = usePlayerName(activePlayer)
+  const selectedTile=(rules?.remind(Memory.TilePreview) !== undefined)
 
   if (playerId !== undefined && activePlayer === playerId) {
-    return <>{t('header.choose.tile.you')}</>
+    if (selectedTile)
+      return <>{t('header.choose.tile.you.orient')}</>
+    return <>{t('header.choose.tile.you.place')}</>
   } else {
     return <>{t('header.choose.tile.player', { player })}</>
   }
