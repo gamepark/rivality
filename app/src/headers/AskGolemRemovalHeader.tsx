5a6f12cb08c9e33cccadd49c47b8aeca49d35487
Denis <denis.boulanger@yahoo.com>
2024-07-06 13:53:42 +0200

    Improved header to ask golem removal

--- app/src/headers/AskGolemRemovalHeader.tsx
+++ app/src/headers/AskGolemRemovalHeader.tsx
@@ -1,18 +1,19 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { Picture } from '@gamepark/react-game'
 import { CustomMoveType } from '@gamepark/rivality/rules/CustomMoveType'
 import { RivalityRules } from '@gamepark/rivality/RivalityRules'
 import { PlayMoveButton, useLegalMove, usePlayerName, usePlayerId, useRules } from '@gamepark/react-game'
 import { isCustomMoveType } from '@gamepark/rules-api'
 import { Trans, useTranslation } from 'react-i18next'
+import removeGolem1Icon from '../images/icon/no_golem1.png'
+import removeGolem2Icon from '../images/icon/no_golem2.png'
+import removeGolem3Icon from '../images/icon/no_golem3.png'
 
 export const AskGolemRemovalHeader = () => {
   const p1 = useLegalMove(isCustomMoveType(CustomMoveType.Player1))
   const p2 = useLegalMove(isCustomMoveType(CustomMoveType.Player2))
   const p3 = useLegalMove(isCustomMoveType(CustomMoveType.Player3))
-
-  const name1=usePlayerName(1)
-  const name2=usePlayerName(2)
-  const name3=usePlayerName(3)
 
   const { t } = useTranslation()
   const playerId = usePlayerId()
@@ -20,12 +21,27 @@ export const AskGolemRemovalHeader = () => {
   const player = usePlayerName(activePlayer)
 
   if (playerId !== undefined && activePlayer === playerId) {
-    return <><Trans defaults="header.ask.golem.removal.you"></Trans>:
-    <PlayMoveButton move={p1}>{name1}</PlayMoveButton>
-    <PlayMoveButton move={p2}>{name2}</PlayMoveButton>
-    <PlayMoveButton move={p3}>{name3}</PlayMoveButton>
+    return <><Trans defaults="header.ask.golem.removal.you"></Trans>&nbsp;
+    {p1 !== undefined && <PlayMoveButton move={p1} css={buttonCss}>
+      <Picture css={iconCss} src={removeGolem1Icon}/>
+    </PlayMoveButton>}
+    {p2 !== undefined && <PlayMoveButton move={p2} css={buttonCss}>
+      <Picture css={iconCss} src={removeGolem2Icon}/>
+    </PlayMoveButton>}
+    {p3 !== undefined && <PlayMoveButton move={p3} css={buttonCss}>
+      <Picture css={iconCss} src={removeGolem3Icon}/>
+    </PlayMoveButton>}
     </>
   } else {
     return <>{t('header.ask.golem.removal.player', { player })}</>
   }
 }
+
+const iconCss=css`
+  vertical-align: top;
+  max-height: 1em;
+`
+
+const buttonCss=css`
+  background-color: #27295c;
+`
