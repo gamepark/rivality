0b5d6e225abf70f3318c85eef5563d0ee9c8b301
Romain Fromi <romain.fromi@gmail.com>
2024-07-08 11:09:17 +0200

    Remove PlayerColor file

--- app/src/locators/Locators.tsx
+++ app/src/locators/Locators.tsx
@@ -1,14 +1,14 @@
+import { ItemLocator } from '@gamepark/react-game'
 import { LocationType } from '@gamepark/rivality/material/LocationType'
 import { MaterialType } from '@gamepark/rivality/material/MaterialType'
-import { PlayerColor } from '@gamepark/rivality/PlayerColor'
-import { ItemLocator } from '@gamepark/react-game'
+import { PlayerId } from '@gamepark/rivality/PlayerId'
 import { boardLocator } from './BoardLocator'
-import { tileButtonLocator } from './TileButtonLocator'
+import { playerDeckLocator } from './PlayerDeckLocator'
 import { playerGolemStackLocator } from './PlayerGolemStackLocator'
 import { playerHandLocator } from './PlayerHandLocator'
-import { playerDeckLocator } from './PlayerDeckLocator'
+import { tileButtonLocator } from './TileButtonLocator'
 
-export const Locators: Partial<Record<LocationType, ItemLocator<PlayerColor, MaterialType, LocationType>>> = {
+export const Locators: Partial<Record<LocationType, ItemLocator<PlayerId, MaterialType, LocationType>>> = {
   [LocationType.PlayerHand]: playerHandLocator,
   [LocationType.PlayerDeck]: playerDeckLocator,
   [LocationType.Board]: boardLocator,
--- rules/src/PlayerColor.ts
+++
@@ -1,9 +0,0 @@
-import { isEnumValue } from '@gamepark/rules-api'
-
-export enum PlayerColor {
-  Pink   = 1,
-  Orange = 2,
-  Green  = 3
-}
-
-export const playerColors = Object.values(PlayerColor).filter(isEnumValue)
