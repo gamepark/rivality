0f81c29b1f49e85afc491e54396bdc5e986ce4d8
Denis <denis.boulanger@yahoo.com>
2024-07-13 20:25:05 +0200

    Adapted animation speed

--- app/src/RivalityAnimations.ts
+++ app/src/RivalityAnimations.ts
@@ -1,7 +1,22 @@
 import { MaterialGameAnimations } from '@gamepark/react-game'
 import { MaterialType } from '@gamepark/rivality/material/MaterialType'
-import { isMoveItemType } from '@gamepark/rules-api'
+import { isMoveItemType, isMoveItemTypeAtOnce } from '@gamepark/rules-api'
 
 export const rivalityAnimations = new MaterialGameAnimations()
 
-rivalityAnimations.when().move(move => isMoveItemType(MaterialType.Tile)(move)).mine().duration(0.2)
+rivalityAnimations.when()
+  .move(move => isMoveItemType(MaterialType.Tile)(move))
+  .mine()
+  .duration(0.2)
+
+rivalityAnimations.when()
+  .move((move) => isMoveItemType(MaterialType.Golem)(move))
+  .duration(0.6)
+
+rivalityAnimations.when()
+  .move((move) => isMoveItemTypeAtOnce(MaterialType.Golem)(move))
+  .duration(0.6)
+
+rivalityAnimations.when()
+  .move((move) => isMoveItemType(MaterialType.Wizard)(move))
+  .duration(0.6)
