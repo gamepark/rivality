fafd6e177288e88ab1872e449fb3b877383f8b4d
Romain Fromi <romain.fromi@gmail.com>
2024-08-07 14:07:17 +0200

    Remove unused override before function is removed from framework

--- app/src/material/WizardDescription.tsx
+++ app/src/material/WizardDescription.tsx
@@ -1,7 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { TokenDescription } from '@gamepark/react-game'
 import { PlayerColor } from '@gamepark/rivality/PlayerColor'
-import { MaterialItem } from '@gamepark/rules-api'
 import Wizard1 from '../images/Wizard1.png'
 import Wizard2 from '../images/Wizard2.png'
 import Wizard3 from '../images/Wizard3.png'
@@ -15,10 +14,6 @@ export class WizardDescription extends TokenDescription  {
     [PlayerColor.Purple]: Wizard1,
     [PlayerColor.Orange]: Wizard2,
     [PlayerColor.Green]: Wizard3
-  }
-
-  getRotateZ(_item: MaterialItem): number {
-    return 0
   }
 }
 
